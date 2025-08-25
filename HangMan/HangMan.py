cc5f50e387c776974b554e946b94556cc9923f0a
Athmi <athmik9082gmail.com>
2025-08-25 21:29:08 +0530

    Changes in Hangman

--- HangMan/HangMan.py
+++ HangMan/HangMan.py
@@ -1,23 +1,23 @@
 import random
 list=['hassan','shoaib','zohaib']
 word=random.choice(list)
-print(word)
 letters=[]
 for i in range(10):
-    letter=input('Guess a letter').lower()
-
+    print('You have',7-i,'guesses left')
+    letter=input('Guess a letter:').lower()
     disp=''
     for i in word:
-
-        if i == letter:
-            disp+=i
-            letters.append(letter)
-        elif i in letters:
+        if i == letter or i in letters:
             disp+=i
         else:
             disp+='_'
-
+    if letter not in letters:
+        letters.append(letter)
     print(disp)
     if '_' not in disp:
         game=True
         print('You Win')
+        break
+if '_' in disp:
+    print('You Lose')
+    print('The word was',word)
