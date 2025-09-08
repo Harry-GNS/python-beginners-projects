8e9e0675c5adfd61b8504766804061d7c2fc02c6
Ofek-Hodis <ofkhod@gmail.com>
2025-09-08 17:55:35 +0300

    Improved and fully functioning Hang Man logic

--- HangMan/HangMan.py
+++ HangMan/HangMan.py
@@ -1,23 +1,41 @@
 import random
-list=['hassan','shoaib','zohaib']
-word=random.choice(list)
-print(word)
-letters=[]
-for i in range(10):
-    letter=input('Guess a letter').lower()
+abc = 'abcdefghijklmnopqrstuvwxyz'
+word_list = ['omelette','computer','cat', 'bottle', 'vase', 'space', 'fingers', 'bracelet', 'snake', 'jewel', 'lake', 'united', 'shoe', 'cannon', 'peace', 'cheese', 'baby']
+word = random.choice(word_list)
+used_letters= ''
+display = '_' * len(word)
 
-    disp=''
-    for i in word:
-
-        if i == letter:
-            disp+=i
-            letters.append(letter)
-        elif i in letters:
-            disp+=i
+i = 0
+while i in range(7):  # User has 8 guesses (i is 0 based)
+    while True:  # Loop to verify the input is a new letter
+        input_letter = input('Guess a letter: ').lower()
+        if input_letter in used_letters:
+            print('You already guessed that letter. Please try again.')
+        elif input_letter in abc:
+            break
         else:
-            disp+='_'
+            print('Invalid input. Please type a letter: ')
+    letter_found = False  # Resetting the value for every new guess
 
-    print(disp)
-    if '_' not in disp:
-        game=True
-        print('You Win')
+    for n in range(len(word)):
+        if input_letter == word[n]:
+            display = display[:n] + input_letter + display[n+1:]  # Replacing character in n index with right letter
+            letter_found = True
+
+    if letter_found:
+        print("Good guess!")
+    else:
+        print("Incorrect letter")
+        i+=1
+    if display == word:
+        print("You guessed the word! You had " + str(7-i) + " guesses left")
+        break
+
+    used_letters = used_letters + input_letter  # Saving used letters
+    print(display)
+    print("Letters you already guessed: " + used_letters)
+    print("Remaining guesses: " + str(7 - i))
+    print("")  # Spacing out the text output
+
+    if not i in range(7):
+        print("Sorry, the word was: " + word)
